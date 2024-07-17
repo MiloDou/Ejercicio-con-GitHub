229743acc5d1547e39a813b96c0ade186adad7fe
MiloDou <158305280+MiloDou@users.noreply.github.com>
2024-07-16 22:01:42 -0600

    Formato base del programa

--- Ejercicio con GitHub/Program.cs
+++ Ejercicio con GitHub/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("---------------------------------");
+Console.WriteLine("             WELCOME");
+Console.WriteLine("---------------------------------");
+while(true)
+{
+    try
+    {
+        Console.Clear();
+        int option;
+        Console.WriteLine("Choose a option: ");
+        option = Convert.ToInt32(Console.ReadLine());
+        Console.ReadKey();
+        switch (option)
+        {
+            case 1:
+                while (true)
+                {
+
+                    {
+                        
+                        try
+                        {
+                            Console.Clear ();
+
+                        }
+                        catch
+                        {
+
+                        }
+
+
+                    }
+                    Console.ReadKey ();
+                }
+
+                break;
+        }
+    }
+    catch(FormatException)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid Format");
+        Console.ResetColor();
+
+    }
+
+}
